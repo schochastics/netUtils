1b601a36219800f5a64dca2472eb47ec32b7862c
schochastics <david@schochastics.net>
2020-03-16 21:10:50 +0000

    added

---
+++ R/graphs.R
@@ -0,0 +1,54 @@
+#' @title k partite graphs
+#' @description  Create a random k-partite graph.
+#'
+#' @param n number of nodes
+#' @param grp vector of partition sizes
+#' @return kpartite graph
+#' @author David Schoch
+#' @export
+
+graph_kpartite <- function(n=10,grp=c(5,5)){
+  g <- igraph::graph.empty(n=n,directed=FALSE)
+  cur_node <- 1
+  nodes <- 1:n
+  for(i in 1:(length(grp)-1)){
+    add_nodes <- cur_node:(cur_node + grp[i] - 1)
+    add_edges <- c(t(expand.grid(add_nodes,nodes[nodes>max(add_nodes)])))
+    g <- igraph::add.edges(g,add_edges)
+    cur_node <- cur_node + grp[i]
+  }
+  return(g)
+}
+
+#' @title convert igraph object to sage format
+#' @description  convert igraph object to sage format
+#'
+#' @param g igraph object
+#' @details
+#' sage code
+#'
+#' gis = g.is_interval(certificate=true)
+#'
+#'gis
+#'
+#'gisstr=str(gis)
+#'
+#'o = open('interval_raw.txt','w')
+#'
+#'o.write(gisstr)
+#'
+#'o.close()
+#'
+#'system('grep -oe "[0-9]\\+,\\s[0-9]\\+" interval_raw.txt > intervals.txt')
+#' @return sage string
+#' @author David Schoch
+#' @export
+
+graph_to_sage <- function(g){
+  igraph::V(g)$name <- 1:igraph::vcount(g)
+  tst <- igraph::get.adjlist(g)
+  gstr <- sapply(1:igraph::vcount(g),function(x)paste0(x,":","[",paste(tst[[x]],collapse = ","),"]"))
+  gstr <- paste(gstr,collapse = ",")
+  gstr <- paste0("g=Graph({",gstr,"})")
+  gstr
+}
