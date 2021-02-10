2d3e761d56cafc321b0c508e853759a5e7676124
schochastics <david@schochastics.net>
2021-02-10 21:56:22 +0000

    forgot to actually add the files...

---
+++ R/core_periphery.R
@@ -0,0 +1,92 @@
+#' Discrete core-periphery model
+#' @description Fits a discrete core-periphery model to a give network
+#' @param graph igraph object.
+#' @param method algorithm to use (see details)
+#' @param iter number of iterations for SA optimization
+#' @details The function fits the data to an optimal pattern matrix with simulated annealing (method="SA") or a rank 1 approximation (method="rk1"). The rank 1 approximation is computationally far cheaper but also highly experimental. Best is to compare the results from both models.
+#' @return list with numeric vector with entries (k1,k2,...ki...) where ki assigns vertex i to either the core (ki=1) or periphery (ki=0), and the maximal correlation with an optimal pattern matrix
+#' @references
+#' Borgatti, Stephen P., and Martin G. Everett. "Models of core/periphery structures." Social networks 21.4 (2000): 375-395.
+#' @author David Schoch
+#' @export
+core_periphery <- function(graph,method="SA",iter=50000){
+  if(method=="SA"){
+    A <- igraph::as_adj(graph,type = "both",sparse = FALSE)
+    n <- nrow(A)
+    cvec <- sample(0:1,n,replace = TRUE)
+    res <- stats::optim(par = cvec, fn = cp_fct1__0, A = A,gr = genperm,method = "SANN",
+                 control = list(maxit = iter, temp = 10, tmax = 100, trace = FALSE,
+                                REPORT = 5))
+    return(list(vec=res$par,corr=res$value))
+  } else if(method=="rk1"){
+    ev <- igraph::evcent(graph)$vector
+
+    thresh <- unique(ev)
+    optcorr <- -2
+
+    for(tr in thresh){
+      evabs <- (ev>=tr)+0
+      E <- outer(evabs,evabs,"+")
+      E[E==1] <- NA
+      E[E==2] <- 1
+      diag(E) <- 0
+      if(sum(E,na.rm = TRUE)==0){
+        next()
+      }
+      tmp <- graph_cor(E,A)
+      if(tmp>optcorr){
+        optperm <- evabs
+        optcorr <- tmp
+      }
+    }
+    return(list(vec = optperm,corr=optcorr))
+  } else{
+    stop("method must be one of 'SA' and 'rk1'")
+  }
+}
+
+
+#helper functions ----
+cp_fct1110 <- function(A,cvec){ #core=1 periphery=0
+  delta <- outer(cvec,cvec,function(x,y) x==1 | y==1 )
+  -sum(A*delta,na.rm = TRUE)
+}
+
+cp_fct1__0 <- function(A,cvec){ #core=1 periphery=0
+  delta <- outer(cvec,cvec,function(x,y) x+y)
+  delta[delta==1] <- NA
+  delta[delta==2] <- 1
+  # -sum(A*delta,na.rm = TRUE)
+  -graph_cor(delta,A)
+}
+
+genperm <- function(A,cvec){
+  # 1=switch between values, 2= switch two nodes
+  what <- sample(1:2,1,prob = c(0.5,0.5))
+  if(what==1){
+    v <- sample(1:length(cvec),1)
+    cvec[v] <- 1-cvec[v]
+  } else if(what==2){
+    core <- which(cvec==1)
+    pery <- which(cvec==0)
+    v <- sample(core,1)
+    w <- sample(pery,1)
+    cvec[v] <- 0
+    cvec[w] <- 1
+  } else{
+
+  }
+  cvec
+}
+
+
+genperm_switch <- function(A,cvec){
+  core <- which(cvec==1)
+  pery <- which(cvec==0)
+  v <- sample(core,1)
+  w <- sample(pery,1)
+  cvec[v] <- 0
+  cvec[w] <- 1
+  cvec
+}
+
---
+++ R/qap.R
@@ -0,0 +1,42 @@
+#' Convert graphs to graph6 symbols
+#'
+#' This function converts graphs to graph6 symbols. Implemented methods expect
+#' the graph to be an adjacency matrix, an igraph, or a network object.
+#'
+#' @param object1  igraph object or adjacency matrix
+#' @param object2  igraph object or adjacency matrix over the same vertex set as object1
+#'
+#' @return correlation between graphs
+#'
+#' @export
+graph_cor <- function(object1,object2) UseMethod("graph_cor")
+
+#' @rdname graph_cor
+#' @method graph_cor default
+#' @export
+graph_cor.default <- function(object1,object2) {
+  stop("don't know how to handle class ", dQuote(data.class(object1)), " and ", dQuote(data.class(object2)))
+}
+
+#' @rdname graph_cor
+#' @method graph_cor igraph
+#' @export
+graph_cor.igraph <- function(object1,object2){
+  A1 <- igraph::as_adj(object1,type = "both",sparse = FALSE)
+  A2 <- igraph::as_adj(object2,type = "both",sparse = FALSE)
+  graph_cor.matrix(A1,A2)
+}
+
+#' @rdname graph_cor
+#' @method graph_cor matrix
+#' @export
+graph_cor.matrix <- function(object1,object2){
+  stats::cor(c(object1),c(object2),use = "complete.obs")
+}
+
+#' @rdname graph_cor
+#' @method graph_cor array
+#' @export
+graph_cor.array <- function(object1,object2){
+  stats::cor(c(object1),c(object2),use = "complete.obs")
+}
