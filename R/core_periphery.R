5f7c75ad232a1890ff991dd2bc96d8020d35fa82
schochastics <david@schochastics.net>
2021-03-07 21:16:13 +0000

    added degree based rank 1 approximation

--- R/core_periphery.R
+++ R/core_periphery.R
@@ -3,13 +3,13 @@
 #' @param graph igraph object.
 #' @param method algorithm to use (see details)
 #' @param iter number of iterations for SA optimization
-#' @details The function fits the data to an optimal pattern matrix with simulated annealing (method="SA") or a rank 1 approximation (method="rk1"). The rank 1 approximation is computationally far cheaper but also more experimental. Best is to compare the results from both models.
+#' @details The function fits the data to an optimal pattern matrix with simulated annealing (method="SA") or a rank 1 approximation, either with degree centrality (method="rk1_dc") or eigenvector centrality (method="rk1_ec") . The rank 1 approximation is computationally far cheaper but also more experimental. Best is to compare the results from both models.
 #' @return list with numeric vector with entries (k1,k2,...ki...) where ki assigns vertex i to either the core (ki=1) or periphery (ki=0), and the maximal correlation with an optimal pattern matrix
 #' @references
 #' Borgatti, Stephen P., and Martin G. Everett. "Models of core/periphery structures." Social networks 21.4 (2000): 375-395.
 #' @author David Schoch
 #' @export
-core_periphery <- function(graph,method="SA",iter=5000){
+core_periphery <- function(graph,method="rk1_dc",iter=5000){
   A <- igraph::as_adj(graph,type = "both",sparse = FALSE)
   if(method=="SA"){
     n <- nrow(A)
@@ -18,7 +18,32 @@ core_periphery <- function(graph,method="SA",iter=5000){
                  control = list(maxit = iter, temp = 10, tmax = 100, trace = FALSE,
                                 REPORT = 5))
     return(list(vec=res$par,corr=-res$value))
-  } else if(method=="rk1"){
+  } else if(method=="rk1_dc"){
+    ev <- igraph::degree(graph,mode="all",loops = FALSE)
+
+    thresh <- unique(ev)
+    optcorr <- -2
+
+    for(tr in thresh){
+      evabs <- (ev>=tr)+0
+      E <- outer(evabs,evabs,"+")
+      E[E==1] <- NA
+      E[E==2] <- 1
+      diag(E) <- NA
+      if(sum(E,na.rm = TRUE)==0){
+        next()
+      }
+      tmp <- suppressWarnings(graph_cor(E,A))
+      if(is.na(tmp)){
+        next()
+      }
+      if(tmp>optcorr){
+        optperm <- evabs
+        optcorr <- tmp
+      }
+    }
+    return(list(vec = optperm,corr=optcorr))
+  }else if(method=="rk1_ec"){
     ev <- round(igraph::evcent(graph)$vector,8)
 
     thresh <- unique(ev)
--- tests/testthat/test-core_periphery.R
+++ tests/testthat/test-core_periphery.R
@@ -2,7 +2,9 @@ test_that("core periphery works", {
   library(igraph)
   g <- graph.full(5,directed = FALSE)
   g <- add.vertices(g,5)
-  vec1 <- core_periphery(g,method="rk1")$vec
+  vec1 <- core_periphery(g,method="rk1_ec")$vec
   expect_equal(vec1,c(1,1,1,1,1,0,0,0,0,0))
+  vec2 <- core_periphery(g,method="rk1_dc")$vec
+  expect_equal(vec2,c(1,1,1,1,1,0,0,0,0,0))
   expect_error(core_periphery(graph_empty,method = "l33t"))
 })
